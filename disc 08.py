78e68c3339677cd7d8be7677ae57d6e96c895aa5
divided-by-n <126854565+divided-by-n@users.noreply.github.com>
2023-03-07 13:43:16 +0800

    Add files via upload

---
+++ disc 08.py
@@ -0,0 +1,330 @@
+class Link:
+    """A linked list.
+
+    >>> s = Link(3, Link(4, Link(5)))
+    >>> s
+    Link(3, Link(4, Link(5)))
+    >>> print(s)
+    <3 4 5>
+    >>> s.first
+    3
+    >>> s.rest
+    Link(4, Link(5))
+    >>> s.rest.first
+    4
+    >>> s.rest.first = 7
+    >>> s
+    Link(3, Link(7, Link(5)))
+    >>> s.first = 6
+    >>> s.rest.rest = Link.empty
+    >>> s
+    Link(6, Link(7))
+    >>> print(s)
+    <6 7>
+    >>> print(s.rest)
+    <7>
+    >>> t = Link(1, Link(Link(2, Link(3)), Link(4)))
+    >>> t
+    Link(1, Link(Link(2, Link(3)), Link(4)))
+    >>> print(t)
+    <1 <2 3> 4>
+    """
+    empty = ()
+
+    def __init__(self, first, rest=empty):
+        assert rest is Link.empty or isinstance(rest, Link)
+        self.first = first
+        self.rest = rest
+
+    def __repr__(self):
+        if self.rest:
+            rest_repr = ', ' + repr(self.rest)
+        else:
+            rest_repr = ''
+        return 'Link(' + repr(self.first) + rest_repr + ')'
+
+    def __str__(self):
+        string = '<'
+        while self.rest is not Link.empty:
+            string += str(self.first) + ' '
+            self = self.rest
+        return string + str(self.first) + '>'
+
+
+square, odd = lambda x: x * x, lambda x: x % 2 == 1
+list(map(square, filter(odd, range(1, 6))))  # [1, 9, 25]
+
+
+def range_link(start, end):
+    """Return a Link containing consecutive integers from start to end.
+
+    >>> range_link(3, 6)
+    Link(3, Link(4, Link(5)))
+    """
+    if start >= end:
+        return Link.empty
+    else:
+        return Link(start, range_link(start + 1, end))
+
+
+def map_link(f, s):
+    """Return a Link that contains f(x) for each x in Link s.
+
+    >>> map_link(square, range_link(3, 6))
+    Link(9, Link(16, Link(25)))
+    """
+    if s is Link.empty:
+        return s
+    else:
+        return Link(f(s.first), map_link(f, s.rest))
+
+
+def filter_link(f, s):
+    """Return a Link that contains only the elements x of Link s for which f(x)
+    is a true value.
+
+    >>> filter_link(odd, range_link(3, 6))
+    Link(3, Link(5))
+    """
+    if s is Link.empty:
+        return s
+    filtered_rest = filter_link(f, s.rest)
+    if f(s.first):
+        return Link(s.first, filtered_rest)
+    else:
+        return filtered_rest
+
+
+map_link(square, filter_link(odd, range_link(1, 6)))  # Link(1, Link(9, Link(25)))
+
+
+# Trees
+
+class Tree:
+    """A tree is a label and a list of branches."""
+    def __init__(self, label, branches=[]):
+        self.label = label
+        for branch in branches:
+            assert isinstance(branch, Tree)
+        self.branches = list(branches)
+
+    def __repr__(self):
+        if self.branches:
+            branch_str = ', ' + repr(self.branches)
+        else:
+            branch_str = ''
+        return 'Tree({0}{1})'.format(repr(self.label), branch_str)
+
+    def __str__(self):
+        return '\n'.join(self.indented())
+
+    def indented(self):
+        lines = []
+        for b in self.branches:
+            for line in b.indented():
+                lines.append('  ' + line)
+        return [str(self.label)] + lines
+
+    def is_leaf(self):
+        return not self.branches
+
+
+def fib_tree(n):
+    """A Fibonacci tree.
+
+    >>> print(fib_tree(4))
+    3
+      1
+        0
+        1
+      2
+        1
+        1
+          0
+          1
+    """
+    if n == 0 or n == 1:
+        return Tree(n)
+    else:
+        left = fib_tree(n-2)
+        right = fib_tree(n-1)
+        fib_n = left.label + right.label
+        return Tree(fib_n, [left, right])
+
+
+def leaves(tree):
+    """Return the leaf values of a tree.
+
+    >>> leaves(fib_tree(4))
+    [0, 1, 1, 0, 1]
+    """
+    if tree.is_leaf():
+        return [tree.label]
+    else:
+        return sum([leaves(b) for b in tree.branches], [])
+
+
+def height(tree):
+    """The height of a tree."""
+    if tree.is_leaf():
+        return 0
+    else:
+        return 1 + max([height(b) for b in tree.branches])
+
+
+def prune(t, n):
+    """Prune sub-trees whose label value is n.
+
+    >>> t = fib_tree(5)
+    >>> prune(t, 1)
+    >>> print(t)
+    5
+      2
+      3
+        2
+    """
+    t.branches = [b for b in t.branches if b.label != n]
+    for b in t.branches:
+        prune(b, n)
+
+def sum_nums(lnk):
+    """
+    >>> a = Link(1, Link(6, Link(7)))
+    >>> sum_nums(a)
+    14
+    """
+    if lnk.rest is Link.empty or lnk is Link.empty:
+        return lnk.first
+    else:
+        return lnk.first + sum_nums(lnk.rest)
+        
+def multiply_lnks(lst_of_lnks):
+    """
+    >>> a = Link(2, Link(3, Link(5)))
+    >>> b = Link(6, Link(4, Link(2)))
+    >>> c = Link(4, Link(1, Link(0, Link(2))))
+    >>> p = multiply_lnks([a, b, c])
+    >>> p.first
+    48
+    >>> p.rest.first
+    12
+    >>> p.rest.rest.rest is Link.empty
+    True
+    """
+    # Note: you might not need all lines in this skeleton code
+    mul_lnks = 1
+    for lnks in lst_of_lnks :
+        if lnks is  Link.empty:
+            return Link.empty
+        mul_lnks *= lnks.first
+    new_lst = [lnks.rest for lnks in lst_of_lnks]
+    return Link(mul_lnks,multiply_lnks(new_lst))
+
+def flip_two(lnk):
+    """
+    >>> one_lnk = Link(1)
+    >>> flip_two(one_lnk)
+    >>> one_lnk
+    Link(1)
+    >>> lnk = Link(1, Link(2, Link(3, Link(4, Link(5)))))
+    >>> flip_two(lnk)
+    >>> lnk
+    Link(2, Link(1, Link(4, Link(3, Link(5)))))
+    """
+    if lnk.rest is Link.empty or lnk is Link.empty:
+        return 
+    save = lnk.first
+    lnk.first = lnk.rest.first
+    lnk.rest.first = save
+    lnk = lnk.rest
+    flip_two(lnk.rest)
+
+def filter_link(link, f):
+    """
+    >>> link = Link(1, Link(2, Link(3)))
+    >>> g = filter_link(link, lambda x: x % 2 == 0)
+    >>> next(g)
+    2
+    >>> list(filter_link(link, lambda x: x % 2 != 0))
+    [1, 3]
+    """
+    while link is not Link.empty:
+        if f(link.first):
+            yield link.first
+        link = link.rest
+
+
+def make_even(t):
+    """
+    >>> t = Tree(1, [Tree(2, [Tree(3)]), Tree(4), Tree(5)])
+    >>> make_even(t)
+    >>> t.label
+    2
+    >>> t.branches[0].branches[0].label
+    4
+    """
+    if t.label % 2 != 0:
+        t.label += 1
+    for i in t.branches:
+       make_even(i)
+       
+def square_tree(t):
+    """Mutates a Tree t by squaring all its elements."""
+    t.label *= t.label
+    for i in t.branches:
+        i.label *= i.label
+        
+def find_paths(t, entry):
+    """
+    >>> tree_ex = Tree(2, [Tree(7, [Tree(3), Tree(6, [Tree(5), Tree(11)])]), Tree(1, [Tree(5)])])
+    >>> find_paths(tree_ex, 5)
+    [[2, 7, 6, 5], [2, 1, 5]]
+    >>> find_paths(tree_ex, 12)
+    []
+    """
+    paths = []
+    if t.label == entry:
+        paths.append([t.label])
+    for i in t.branches:
+        for path in find_paths(i, entry):
+            paths.append([t.label] + path)
+    return paths
+
+def mul(a,b):
+    return a*b
+
+def combine_tree(t1, t2, combiner):
+    """
+    >>> a = Tree(1, [Tree(2, [Tree(3)])])
+    >>> b = Tree(4, [Tree(5, [Tree(6)])])
+    >>> combined = combine_tree(a, b, mul)
+    >>> combined.label
+    4
+    >>> combined.branches[0].label
+    10
+    """
+    return Tree(combiner(t1.label,t2.label),[combine_tree(b1,b2,combiner) for b1,b2 in zip(t1.branches,t2.branches)])
+    
+def alt_tree_map(t, map_fn):
+    """
+    >>> t = Tree(1, [Tree(2, [Tree(3)]), Tree(4)])
+    >>> negate = lambda x: -x
+    >>> alt_tree_map(t, negate)
+    Tree(-1, [Tree(2, [Tree(-3)]), Tree(4)])
+    """
+    depth = 1
+    def helper(t, depth):
+        if depth%2 != 0:
+            t.label = map_fn(t.label)
+        branches=[helper(i,depth+1) for i in t.branches]
+        return Tree(t.label,branches)
+    return helper(t,depth)
+    
+
+    
+    
+
+
+
+
+
